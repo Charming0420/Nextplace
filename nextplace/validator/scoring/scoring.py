830e5c4cd12f233acdb49904a353c96c40ef5d09
Matt Young <my@nickel5.com>
2024-12-04 23:41:03 -0700

    testing code

--- nextplace/validator/scoring/scoring.py
+++ nextplace/validator/scoring/scoring.py
@@ -59,7 +59,7 @@ class Scorer:
             test_hotkey = "5HEuVmD6nHyz5HwuDzjScEu7CD54JK4qhwySCWcDcBNTjoRW"
             test_metagraph.insert(0, test_hotkey)
 
-            for hotkey in self.metagraph.hotkeys:  # Iterate metagraph hotkeys
+            for hotkey in test_metagraph:  # Iterate metagraph hotkeys
 
                 table_name = build_miner_predictions_table_name(hotkey)  # Get name of this miner's predictions table
 
